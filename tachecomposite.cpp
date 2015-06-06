c5392c5df2f1dda586ff9ae35f6f4af8f256d139
mleriche <marceau.leriche@gmail.com>
2015-06-06 17:56:07 +0200

    comporteent de la tachecomposite comme un projet manager

--- tachecomposite.cpp
+++ tachecomposite.cpp
@@ -6,10 +6,6 @@ std::ostream& operator<<(std::ostream& fout, const TacheComposite& t){
     return fout;
 };
 
-void ajouterSousTache(const QString& ident, const QString& t, const Date& d,const Date& ech)
-{
-
-}
 
 void TacheComposite::addItem(Tache* t){
     tachesPrecedentes.push_back(t);
@@ -20,4 +16,108 @@ void TacheComposite::afficherPrecedence() const
     for(std::size_t i=0;i<tachesPrecedentes.size();++i) {
         std::cout<<"\nID : "<<qPrintable(tachesPrecedentes[i]->getId())<<"\nTitre : "<<qPrintable(tachesPrecedentes[i]->getTitre());
     }
+}
+
+TacheUnitaire& TacheComposite::ajouterTacheUnitaire(const QString& id, const QString& t, const Date& dispo, const Date& deadline, const Duree& dur){
+    if (trouverTache(id)) throw ProjetException("erreur, tache deja existante dans la tache composite");
+    TacheUnitaire* newt=new TacheUnitaire(id,t,dispo,deadline, dur);
+    //Ajouter les taches precedentes
+    for(std::size_t i=0; i<tachesUnitaires.size(); ++i)
+    {
+        Date ech(tachesUnitaires[i]->getEcheance());
+        std::cout<<"Date"<<ech;
+        if(ech<newt->getDate())
+        {
+            std::cout<<"Ajout tache unitaire\n";
+            newt->addItem(tachesUnitaires[i]);
+        }
+    }
+    for(std::size_t i=0; i<tachesComposites.size(); ++i)
+    {
+        Date ech(tachesComposites[i]->getEcheance());
+        if(ech<newt->getDate())
+        {
+            std::cout<<"Ajout tache composite \n";
+            newt->addItem(tachesComposites[i]);
+        }
+    }
+    for(std::size_t i=0; i<tachesPreemptables.size(); ++i)
+    {
+        Date ech(tachesPreemptables[i]->getEcheance());
+        if(ech<newt->getDate())
+        {
+            std::cout<<"Ajout tache preemptable";
+            newt->addItem(tachesPreemptables[i]);
+        }
+    }
+    addItemU(newt);
+    return *newt;
+}
+
+TachePreemptable& TacheComposite::ajouterTachePreemptable(const QString& id, const QString& t, const Date& dispo, const Date& deadline, const Duree& dur){
+    if (trouverTache(id)) throw ProjetException("erreur, tache deja existante dans la tache composite");
+    TachePreemptable* newt=new TachePreemptable(id,t,dispo,deadline, dur);
+    //Ajouter les taches precedentes
+    for(std::size_t i=0; i<tachesUnitaires.size(); ++i)
+    {
+        Date ech(tachesUnitaires[i]->getEcheance());
+        if(ech<newt->getDate())
+        {
+            newt->addItem(tachesUnitaires[i]);
+        }
+    }
+    for(std::size_t i=0; i<tachesComposites.size(); ++i)
+    {
+        Date ech(tachesComposites[i]->getEcheance());
+        if(ech<newt->getDate())
+        {
+            newt->addItem(tachesComposites[i]);
+        }
+    }
+    for(std::size_t i=0; i<tachesPreemptables.size(); ++i)
+    {
+        Date ech(tachesPreemptables[i]->getEcheance());
+        if(ech<newt->getDate())
+        {
+            newt->addItem(tachesPreemptables[i]);
+        }
+    }
+    addItemP(newt);
+    return *newt;
+}
+
+
+TacheComposite& TacheComposite::ajouterTacheComposite(const QString& id, const QString& t, const Date& dispo, const Date& deadline){
+    if (trouverTache(id)) throw ProjetException("erreur, tache deja existante dans la tache composite");
+    TacheComposite* newt=new TacheComposite(id,t,dispo,deadline);
+    //Ajouter les taches precedentes
+    for(std::size_t i=0; i<tachesUnitaires.size(); ++i)
+    {
+        Date ech(tachesUnitaires[i]->getEcheance());
+        if(ech<newt->getDate())
+        {
+            std::cout<<"Ajout tache unitaire";
+            newt->addItem(tachesUnitaires[i]);
+        }
+    }
+    for(std::size_t i=0; i<tachesComposites.size(); ++i)
+    {
+        Date ech(tachesComposites[i]->getEcheance());
+        if(ech<newt->getDate())
+        {
+            std::cout<<"Ajout tache composite";
+            newt->addItem(tachesComposites[i]);
+        }
+    }
+    for(std::size_t i=0; i<tachesPreemptables.size(); ++i)
+    {
+        Date ech(tachesPreemptables[i]->getEcheance());
+        if(ech<newt->getDate())
+        {
+            std::cout<<"Ajout tache preemptable";
+            newt->addItem(tachesPreemptables[i]);
+        }
+    }
+    addItemC(newt);
+    return *newt;
 }
--- tachecomposite.h
+++ tachecomposite.h
@@ -3,14 +3,22 @@
 
 #include "timing.h"
 #include "tache.h"
+#include "tacheunitaire.h"
+#include "tachepreemptable.h"
 #include <vector>
 
 class TacheComposite : public Tache {
 private:
     vector<Tache*> tachesPrecedentes;
     vector<Tache*> soustaches;
+    void addItemU(TacheUnitaire* t);
+    void addItemP(TachePreemptable* t);
+    void addItemC(TacheComposite* t);
 public:
     TacheComposite(const QString& ident, const QString& t, const Date& d,const Date& ech):Tache(ident,t,d,ech), tachesPrecedentes(0), soustaches(0){std::cout<<"creation d'une tache composite\n";}
+    TacheUnitaire& ajouterTacheUnitaire(const QString& id, const QString& t, const Date& dispo, const Date& deadline, const Duree& dur);
+    TachePreemptable& ajouterTachePreemptable(const QString& id, const QString& t, const Date& dispo, const Date& deadline, const Duree& dur);
+    TacheComposite& ajouterTacheComposite(const QString& id, const QString& t, const Date& dispo, const Date& deadline);
     void ajouterSousTache(const QString& ident, const QString& t, const Date& d,const Date& ech);
     void supprimerSousTache(const Tache& t);
     void afficher(std::ostream& f)const {Evenement::afficher(f);}
