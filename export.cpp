c00d42aba96bc705acc7ed923f2a499ab4e8a223
Mathey Alexis <alexis.mathey@etu.utc.fr>
2015-06-07 16:38:49 +0200

    export des projets OK, reste export des precedences et des compositions

--- export.cpp
+++ export.cpp
@@ -1,5 +1,6 @@
 
 #include "export.h"
+#include <typeinfo>
 
 void ExportXML::save(const QString& f){
     //file=f;
@@ -10,33 +11,56 @@ void ExportXML::save(const QString& f){
     QXmlStreamWriter stream(&newfile);
     stream.setAutoFormatting(true);
     stream.writeStartDocument();
+    //Écriture de tous les projets dans une balise <projets>
     stream.writeStartElement("projets");
-    //S'arranger pour que nb et taches (de TacheManager à l'origine) soient reliés à des attributs de Projet
     for(vector<Projet*>::iterator it1 = PM.getProjets().begin(); it1 != PM.getProjets().end(); ++it1){
+        //chaque projet est dans une balise <projet>
         stream.writeStartElement("projet");
         stream.writeTextElement("identificateur",(*it1)->getId());
         stream.writeTextElement("titre",(*it1)->getTitre());
         stream.writeTextElement("description",(*it1)->getDesc());
         stream.writeTextElement("disponibilite",(*it1)->getDispo().toString());
         stream.writeTextElement("echeance",(*it1)->getEcheance().toString());
+        //Liste des taches du projet dans une balise <taches>
+        stream.writeStartElement("taches");
         for (vector<Tache*>::iterator it2 = (*it1)->getTaches().begin() ; it2 != (*it1)->getTaches().end() ; ++it2){
+            //Chaque tache dans une balise <tache>
             stream.writeStartElement("tache");
-            //pour préemptive s'arranger pour trouver si la Tache est du type tachepreemptable
-            //stream.writeAttribute("preemptive", ((*it2)->isPreemptive())?"true":"false");
+            //Met l'attribut preemptive à true si tache préemptable, false sinon
+            if (typeid(*it2) == typeid(TachePreemptable))
+                stream.writeAttribute("preemptive", "true");
+            else
+                stream.writeAttribute("preemptive", "false");
+            //Met l'attribut composite à true si tache composite, false sinon
+            if (typeid(*it2) == typeid(TachePreemptable))
+                stream.writeAttribute("composite", "true");
+            else
+                stream.writeAttribute("composite", "false");
             stream.writeTextElement("identificateur",(*it2)->getId());
             stream.writeTextElement("titre",(*it2)->getTitre());
             stream.writeTextElement("disponibilite",(*it2)->getDate().toString());
             stream.writeTextElement("echeance",(*it2)->getEcheance().toString());
             //Durée uniquement si tache unitaire
-            /*
-            QString str;
-            str.setNum(taches[i]->getDuree().getDureeEnMinutes());
-            stream.writeTextElement("duree",str);
-            */
+            if (typeid(*it2) ==  typeid(TacheUnitaire)){
+                QString str;
+                str.setNum((*it2)->getDuree().getDureeEnMinutes());
+                stream.writeTextElement("duree",str);
+            }
             stream.writeEndElement();
+        }
+        stream.writeEndElement();
+        stream.writeEndElement();
+    }
+    stream.writeEndElement();
+    //Liste des précédences de Taches dans la balise <precedences>
+    stream.writeStartElement("precedences");
+    for(vector<Projet*>::iterator it1 = PM.getProjets().begin(); it1 != PM.getProjets().end(); ++it1){
+        for (vector<Tache*>::iterator it2 = (*it1)->getTaches().begin() ; it2 != (*it1)->getTaches().end() ; ++it2){
+
         }
     }
     stream.writeEndElement();
+
     stream.writeEndDocument();
     newfile.close();
 }
