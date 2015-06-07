d1e5eeca819a3eeb6cd3caa9b067f9421b28ec37
Mathey Alexis <alexis.mathey@etu.utc.fr>
2015-06-07 18:34:26 +0200

    export projets OK. Reste précédences et composites

--- export.cpp
+++ export.cpp
@@ -1,4 +1,4 @@
-
+#include "projetmanager.h"
 #include "export.h"
 #include <typeinfo>
 
@@ -13,7 +13,8 @@ void ExportXML::save(const QString& f){
     stream.writeStartDocument();
     //Écriture de tous les projets dans une balise <projets>
     stream.writeStartElement("projets");
-    for(vector<Projet*>::iterator it1 = PM.getProjets().begin(); it1 != PM.getProjets().end(); ++it1){
+    const vector<Projet*>* projets = PM.getProjets();
+    for(vector<Projet*>::const_iterator it1 = projets->begin(); it1 != projets->end(); ++it1){
         //chaque projet est dans une balise <projet>
         stream.writeStartElement("projet");
         stream.writeTextElement("identificateur",(*it1)->getId());
@@ -23,7 +24,8 @@ void ExportXML::save(const QString& f){
         stream.writeTextElement("echeance",(*it1)->getEcheance().toString());
         //Liste des taches du projet dans une balise <taches>
         stream.writeStartElement("taches");
-        for (vector<Tache*>::iterator it2 = (*it1)->getTaches().begin() ; it2 != (*it1)->getTaches().end() ; ++it2){
+        const vector<Tache*>* taches = (*it1)->getTaches();
+        for (vector<Tache*>::const_iterator it2 = taches->begin() ; it2 != taches->end() ; ++it2){
             //Chaque tache dans une balise <tache>
             stream.writeStartElement("tache");
             //Met l'attribut preemptive à true si tache préemptable, false sinon
@@ -52,6 +54,7 @@ void ExportXML::save(const QString& f){
         stream.writeEndElement();
     }
     stream.writeEndElement();
+    /*
     //Liste des précédences de Taches dans la balise <precedences>
     stream.writeStartElement("precedences");
     for(vector<Projet*>::iterator it1 = PM.getProjets().begin(); it1 != PM.getProjets().end(); ++it1){
@@ -60,7 +63,7 @@ void ExportXML::save(const QString& f){
         }
     }
     stream.writeEndElement();
-
+    */
     stream.writeEndDocument();
     newfile.close();
 }
